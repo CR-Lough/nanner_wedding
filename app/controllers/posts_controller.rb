29b45e7eb5308fc84dd0708ffbbfdd31ee543291
crlough-gitkraken <loughondata@protonmail.com>
2023-08-06 19:39:36 -0700

    create controller and action for controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,13 @@
+class PostsController < ApplicationController
+
+#define action
+def index
+    #then define corresponding view in views/
+    #these view files are created in erb which can take ruby
+    #and display html
+    
+
+end
+
+
+end

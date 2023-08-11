6516ebda0f6462058bc4a2efc2dcd08a03a57b29
crlough-gitkraken <loughondata@protonmail.com>
2023-08-11 15:40:40 -0600

    attempt a tutorial
    https://dakotaleemartinez.com/tutorials/rails-api-active-storage-file-uploads/

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,29 @@
 class PostsController < ApplicationController
 
-#define action
-def index
-    #then define corresponding view in views/
-    #these view files are created in erb which can take ruby
-    #and display html
+    #define action
+    def index
+        #then define corresponding view in views/
+        #these view files are created in erb which can take ruby
+        #and display html
+    end
+
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+          render json: @post, status: :created
+        else
+          render json: @post.errors, status: :unprocessable_entity
+        end
+    end
+
+    # def create
+    #     post = Post.create!(post_params)
+    #     redirect_to post
+    # end
     
-
-end
-
+    private
+        def post_params
+            params.require(:post).permit(:title, images: [])
+        end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    has_many_attached :images
+    def image_urls
+        images.map{|p| Rails.application.routes.url_helpers.url_for(p) }
+    end
 end
